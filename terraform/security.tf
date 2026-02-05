8823f8660cdbae30b951aa858c30fba5be7de995
vignesh-a-nt07 <vignesh.a@navattech.com>
2026-02-05 16:44:15 +0530

    sg update

--- terraform/security.tf
+++ terraform/security.tf
@@ -21,12 +21,11 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_nodes_sg.id]
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
   }
-
 
   egress {
     from_port   = 0
@@ -34,4 +33,5 @@ resource "aws_security_group" "rds_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 }
